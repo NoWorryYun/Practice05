0706695ef93470173bd7e6f250e3ec083ab2aef7
NoWorryYun <dbstjd3201@naver.com>
2022-04-25 00:36:31 +0900

    2022-04-25 account

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -5,12 +5,17 @@ public class Account {
 	//필드
     private String accountNo;
     private int balance;
-    private int money;
+    private int deposit;
+    private int withdraw;
     
     //생성자 작성
     public Account() {}
     public Account(String accountNo) {
     	this.accountNo = accountNo;
+    }
+    public Account(String accountNo, int balance) {
+    	this.accountNo = accountNo;
+    	this.balance = 0;
     }
     
     //GS
@@ -29,17 +34,16 @@ public class Account {
 	}
 
     //필요한 메소드 작성
-	public int deposit(int money) {
-		money = this.money + money;
-		return money;
+	public void deposit(int deposit) {
+		this.balance = this.balance + deposit;
 	}
 	
-	public int withdraw(int money) {
-		money = this.money - money;
-		return money;
+	public void withdraw(int withdraw) {
+		this.balance = this.balance - withdraw;
 	}
 	
 	public void showBalance() {
+		System.out.println(balance);
 	}
 	
 }
