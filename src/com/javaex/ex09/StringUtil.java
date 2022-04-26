d4317fccfed6975e5045f301333bc0a0751cc12e
윤무걱정 <dbstjd3201@naver.com>
2022-04-26 13:51:52 +0900

    2022-04-26 9번 해결

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -7,9 +7,14 @@ public class StringUtil {
 	
 	
 	//일반
-    public static String concatString(){
-       //메소드 내용작성
+    public static String concatString(String[] strArray){
     	
+    	String hero = strArray[0];
+    	//메소드 내용작성
+    	for(int i = 1 ; i < strArray.length ; i++) {
+    		hero = hero + strArray[i];
+    	}
+    	return hero;
     }
 
 }
